7895e00f2bb019e231dbff499e28af7038dd0110
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2022-10-01 09:45:34 +0530

    Update bubblesort.cpp

--- bubble sort/bubblesort.cpp
+++ bubble sort/bubblesort.cpp
@@ -1,38 +1,44 @@
-// C++ program for implementation
-// of Bubble sort
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-// A function to implement bubble sort
+ 
+void swap(int *a, int *b)
+{
+   int temp = *a;
+   *a = *b;
+   *b = temp;
+}
+ 
 void bubbleSort(int arr[], int n)
 {
-	int i, j;
-	for (i = 0; i < n - 1; i++)
-
-		// Last i elements are already
-		// in place
-		for (j = 0; j < n - i - 1; j++)
-			if (arr[j] > arr[j + 1])
-				swap(arr[j], arr[j + 1]);
+   for (int i = 0; i < n-1; i++)
+   { 
+        // last i elements are already at the correct position
+       for (int j = 0; j < n-i-1; j++)
+       {
+           if (arr[j] > arr[j+1])
+               swap(&arr[j], &arr[j+1]);
+       }
+   }
 }
-
-// Function to print an array
-void printArray(int arr[], int size)
+ 
+void printArray(int arr[], int n)
 {
-	int i;
-	for (i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+   for (int i = 0; i < n; i++)
+       cout << arr[i] << " ";
+   cout << "\n";
 }
-
-// Driver code
+ 
 int main()
 {
-	int arr[] = { 5, 1, 4, 2, 8};
-	int N = sizeof(arr) / sizeof(arr[0]);
-	bubbleSort(arr, N);
-	cout << "Sorted array: \n";
-	printArray(arr, N);
-	return 0;
+   int arr[] = {6, 3, 8, 9, 5};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   cout<<"Given Array: ";
+   printArray(arr, n);
+ 
+   bubbleSort(arr, n);
+  
+   cout<<"Sorted Array: ";
+   printArray(arr, n);
+  
+   return 0;
 }
-// This code is contributed by rathbhupendra
